a609cb3d42e7f7f6c8ed6b65953de4aeacb52808
Susmita sharma <sharmasusmita223@gmail.com>
2024-01-18 14:33:37 +0545

    sdfghj

---
+++ Inheritance/Circle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Circle :Shape
+    {
+        //public class Circle : Shape
+        
+            private double radius;
+            public Circle()
+            {// Default constructor
+            }
+            public Circle(int xValue, int yValue, double radiusIn)
+            {
+                // Another constructor
+                X = xValue;// Use the PROPERTIES of the Point class
+                Y = yValue;
+                SetValues(xValue, yValue);  // USE a base class method to set parameters
+                radius = radiusIn;  // Radius
+            }
+            public void setData(int xValue, int yValue, double radiusIn)
+            {
+                SetValues(xValue, yValue);
+                radius = radiusIn;
+            }
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (value > 0.0) radius = value;
+            }
+        }
+        public class Test
+        {
+            public static void Main()
+            {
+                Shape s = new Shape();
+                s.X = 100;
+                s.Y = 100;
+                Console.WriteLine("Point values are " + s.X + " " + s.Y);
+                Circle c = new Circle(200, 200, 20.0);
+                Console.WriteLine("Circle position is " + c.X + " " + c.Y);
+                Console.WriteLine("Circle diameter is " + c.Radius);
+                Console.Read();//  Wait for a key
+            }
+        }
+
+    }
+
+}
+
---
+++ Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Shape
+{
+    private int x;
+    private int y;
+    public Shape()
+    {
+    }
+    public Shape(int xValue, int yValue)
+    {
+        x = xValue;
+        y = yValue;
+    }
+    public int X
+    {  // Set X property	
+        get
+        {
+            return x;
+        }
+        set
+        {
+            x = value;
+        }
+    }
+    public void SetValues(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+        set
+        {
+            y = value;
+        }
+    }
+}
